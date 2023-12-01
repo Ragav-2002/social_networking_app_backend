1f997496eafa048cb58b0c562192881b76e12eb5
Ragav-2002 <sripalasettyraghavendra2002@gmail.com>
2023-12-01 17:51:47 +0530

    created db and server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const name = 'snap'
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const configureDB = require('./config/db')
+app.use(cors())
+app.use(express.json())
+configureDB()
+const port = 4000
+
+app.listen(port,()=>{
+    console.log('server is running on port', port)
+})
