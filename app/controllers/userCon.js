b9f109b0e77e89c5fd5f1507131a0cd6dd9b2cc7
Ragav-2002 <sripalasettyraghavendra2002@gmail.com>
2023-12-02 16:43:03 +0530

    created user control and authentication

---
+++ app/controllers/userCon.js
@@ -0,0 +1,58 @@
+const User = require('../models/userModel')
+const _ = require('lodash')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const userCon = {}
+
+userCon.register = async(req, res) => {
+    const body = _.pick(req.body, ['username', 'email', 'password'])
+    try{
+        const salt = await bcrypt.genSalt()
+        const hashedPass = await bcrypt.hash(body.password, salt)
+        body.password = hashedPass
+        const users = await User.countDocuments()
+        if(users == 0){
+            body.role = 'admin'
+        }else{
+            body.role = 'user'
+        }
+        const user = new User(body)
+        await user.save()
+        res.json('registered successfully')
+    }catch(e){
+        res.status(500).json({errors: 'something went wrong'})
+    }
+}
+
+userCon.login = async(req, res) => {
+    const body = _.pick(req.body, ['email', 'password'])
+    try{
+        const user = await User.findOne({email: body.email})
+        if(!user){
+            return res.status(404).json({errors: [{ msg: 'invalid email or password' }]})
+        }else if(!await bcrypt.compare(body.password, user.password)){
+            return res.status(404).json({errors: [{ msg: 'invalid email or password' }]})
+        }else{
+            const tokenData = {userId: user._id, userRole: user.role}
+            const token = jwt.sign(tokenData, process.env.SECRET)
+            res.json({token: `bearer ${token}`,message: 'login successful'})
+        }
+    }
+    catch(e){
+        res.status(500).json({errors: 'something went wrong'})
+    }
+}
+
+userCon.account = async(req, res)=> {
+    const {userId} = req.user
+    try{
+        const user = await User.findById(userId)
+        res.json(user)
+    }catch(e){
+        res.status(500).json({errors: 'something went wrong'})
+    }
+}
+
+
+module.exports = userCon
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const configureDB = require('./config/db')
+const userCon = require('./app/controllers/userCon')
+const {authUser} = require('./app/middleware/authentication')
 app.use(cors())
 app.use(express.json())
 configureDB()
 const port = 4000
+
+app.post('/api/user/register', userCon.register)
+app.post('/api/user/login', userCon.login)
+app.get('/api/user', authUser, userCon.account)
 
 app.listen(port,()=>{
     console.log('server is running on port', port)
