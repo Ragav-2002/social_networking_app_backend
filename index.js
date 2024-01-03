ab0a0ecc025e20077c0233a2700145c44d51112f
roman-empire-ui <ysrinivas4901@gmail.com>
2024-01-03 13:57:11 +0530

    Increased file size lmt in index

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const upload = multer({
     storage : storage, 
     fileFilter : fileFilter, 
     limits : {
-        fileSize : 10 * 1024 * 1024
+        fileSize : 30 * 1024 * 1024
     }
 })
 
